e7951ca4842a6c5af4b9454d9396fe2faf22ad43
IakonaColobong <jason.colobong@outlook.com>
2022-02-16 19:34:43 -0600

    created fibonacci program

---
+++ CodingChallenge1/CodingChallenge2/Fibonacci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge2
+{
+    class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            //Using the Fibonacci Sequence whose values do not exceed 4 million, find the sum of the even-valued terms
+
+
+            
+            int x = 1;
+            int y = 2;
+
+            for (int fs = 0; fs < 40; fs++)
+            {
+                y = (y + x);
+                x = fs;   
+            }
+            Console.WriteLine(x);
+
+        }
+    }
+}
